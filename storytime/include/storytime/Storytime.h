71c4e0fb5ec533c482446473e5678727cd2621cb
Torbjørn Årdal <torbjorn.aardal@gmail.com>
2020-04-28 11:26:17 +0200

    Added random class

--- storytime/include/storytime/Storytime.h
+++ storytime/include/storytime/Storytime.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Engine.h"
+#include "system/Random.h"
 
 namespace story = storytime;
 namespace st = story;
---
+++ storytime/src/system/Random.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <random>
+
+namespace storytime
+{
+    class Random
+    {
+    private:
+        std::mt19937 generator;
+        std::uniform_int_distribution<std::mt19937::result_type> distribution;
+
+        Random();
+
+        static Random& Get();
+
+    public:
+        Random(Random const&) = delete;
+
+        void operator=(Random const&) = delete;
+
+        static float Float(float min = 0, float max = 1);
+    };
+}
