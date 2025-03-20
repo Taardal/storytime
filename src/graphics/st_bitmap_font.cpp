2972ee62fd9efdff4a4f0695c3cdf05141d0d996
Torbjørn Årdal <torbjorn.aardal@gmail.com>
2025-03-20 19:34:17 +0100

    foo

--- src/graphics/st_bitmap_font.cpp
+++ src/graphics/st_bitmap_font.cpp
@@ -13,18 +13,6 @@ namespace Storytime {
 
         glm::vec3 position = text_config.position;
 
-        //
-        // Problem:
-        // The renderer will offset each quad left and up by half its size to put its origin in the center,
-        // but this will make each quad appear slightly off-position compared to Tiled Editor.
-        //
-        // Solution:
-        // Offset the position of each sprite right and down by half its size so that the quad offsets done by
-        // the renderer will make them appear at the correct position
-        //
-        position.x += size_x / 2;
-        position.y += size_y / 2;
-
         for (int i = 0; i < text.size(); ++i) {
             const char value = text[i];
 
